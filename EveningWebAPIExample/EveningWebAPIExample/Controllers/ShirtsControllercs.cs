195a3371f815354a39a70a2a7c82f5fe8bc1eed9
Louise <wddtechnologies@gmail.com>
2023-12-12 21:21:12 -0500

    First upload of example for Students

---
+++ EveningWebAPIExample/EveningWebAPIExample/Controllers/ShirtsControllercs.cs
@@ -0,0 +1,51 @@
+﻿using EveningWebAPIExample.Models;
+using EveningWebAPIExample.Models.Respositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EveningWebAPIExample.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShirtsControllercs : ControllerBase
+    {
+
+        [HttpGet]
+        public string GetShirts()
+        {
+            return " Reading all the shirts";
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetShirtsById(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+
+            var shirt = ShirtRepository.GetShirtById(id);
+            if (shirt != null)
+            {
+                return NotFound();
+            }
+            return Ok(shirt);
+        }
+    
+        [HttpPost]
+        public string CreateShirt([FromForm] Shirt shirt)
+        {
+            return $"Creating a shirt";
+        }
+        [HttpPut("{id}")]
+        public string UpdateShirt(int id)
+        {
+            return $"Updating shirt: , {id}";
+        }
+        [HttpDelete("{id}")]
+        public string DeleteShirt(int id)
+        {
+            return $"Deleting shirt: , {id}";
+        }
+
+    }
+}
+
---
+++ EveningWebAPIExample/EveningWebAPIExample/Models/Respositories/ShirtRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace EveningWebAPIExample.Models.Respositories
+{
+    public class ShirtRepository
+    {
+        private static List<Shirt> shirts = new List<Shirt>() 
+        {
+        new Shirt{Id = 1, Brand ="Nike", Color = "Blue", Gender = "Men", Price = 30, Size = 10},
+        new Shirt{Id = 2, Brand ="Addias", Color = "Green", Gender = "Men", Price = 40, Size = 8},
+        new Shirt{Id = 3, Brand ="Nike", Color = "Blue", Gender = "Women", Price = 30, Size = 5},
+        new Shirt{Id = 4, Brand ="Upper", Color = "Green", Gender = "Women", Price = 40, Size = 6},
+        new Shirt{Id = 5, Brand ="Lower", Color = "Blue", Gender = "Women", Price = 10, Size = 2}
+
+        };
+        public static bool ShirtExists(int id)
+        {
+            return shirts.Any(x => x.Id == id);
+        }
+        public static Shirt? GetShirtById(int id)
+        {
+            return shirts.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
