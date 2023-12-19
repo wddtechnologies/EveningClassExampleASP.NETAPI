a7f7e08ef227326908cd94ffec7ad7dc61106e8a
Louise <wddtechnologies@gmail.com>
2023-12-18 20:36:25 -0500

    12-18-2023 Updated files

--- EveningWebAPIExample/EveningWebAPIExample/Models/Respositories/ShirtRepository.cs
+++ EveningWebAPIExample/EveningWebAPIExample/Models/Respositories/ShirtRepository.cs
@@ -11,6 +11,10 @@
         new Shirt{Id = 5, Brand ="Lower", Color = "Blue", Gender = "Women", Price = 10, Size = 2}
 
         };
+        public static List<Shirt> GetShirts()
+        {
+            return shirts;
+        }
         public static bool ShirtExists(int id)
         {
             return shirts.Any(x => x.Id == id);
@@ -18,6 +22,40 @@
         public static Shirt? GetShirtById(int id)
         {
             return shirts.FirstOrDefault(x => x.Id == id);
+        }
+        public static Shirt? GetShirtByProperties(string? brand,string? gender, string?color, int? size)
+        {
+            return shirts.FirstOrDefault(x =>
+            !string.IsNullOrWhiteSpace(brand) &&
+            !string.IsNullOrWhiteSpace(x.Brand) &&
+            x.Brand.Equals(brand, StringComparison.OrdinalIgnoreCase) &&
+            !string.IsNullOrWhiteSpace(gender) &&
+            !string.IsNullOrWhiteSpace(x.Gender) &&
+            x.Gender.Equals(gender, StringComparison.OrdinalIgnoreCase) &&
+            !string.IsNullOrWhiteSpace(color) &&
+            !string.IsNullOrWhiteSpace(x.Color) &&
+            x.Color.Equals(color, StringComparison.OrdinalIgnoreCase) &&
+            size.HasValue &&
+            x.Size.HasValue &&
+            size.Value == x.Size.Value
+            );
+        }
+        public static void AddShirt(Shirt shirt)
+        {
+            int maxId = shirts.Max(x => x.Id);
+            shirt.Id = maxId + 1;
+            shirts.Add(shirt);
+        }
+        public static void UpdateShirt(Shirt shirt)
+        {
+            var shirtToUpdate = shirts.First(x => x.Id == shirt.Id);
+            shirtToUpdate.Brand = shirt.Brand;
+            shirtToUpdate.Price = shirt.Price;
+            shirtToUpdate.Size = shirt.Size;
+            shirtToUpdate.Color = shirt.Color;
+            shirtToUpdate.Gender = shirt.Gender;
+
+
         }
     }
 }
