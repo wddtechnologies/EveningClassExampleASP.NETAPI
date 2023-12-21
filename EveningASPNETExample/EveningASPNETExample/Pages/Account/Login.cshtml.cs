ec2cb568a5b908e971960d8dd248ef64a7300617
Louise <wddtechnologies@gmail.com>
2023-12-20 21:17:26 -0500

    New App of ASP.NET Auth

---
+++ EveningASPNETExample/EveningASPNETExample/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+
+namespace EveningASPNETExample.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; } = new Credential();
+
+        public void OnGet()
+        {
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            // verify the credentials
+            if (!ModelState.IsValid) return Page();
+           // creating the security context 
+            if (Credential.UserName == "admin" && Credential.Password == "password")
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@mywebsite.com"),
+                    new Claim("Department","HR")
+                };
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                return RedirectToPage("/Index");
+            }
+            return Page();
+        }
+
+    }
+    public class Credential
+    {
+        [Required]
+        [Display(Description = "User Name")]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
